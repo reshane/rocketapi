017c3182d50c65e7af632116a96f2f8ecc216256
reshane <shanekenny023@gmail.com>
2023-02-24 18:16:48 -0500

    Prune boring endpoints

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,4 @@
 use rocket::tokio::time::{sleep, Duration};
-use std::io;
-use rocket::tokio::task::spawn_blocking;
 use rocket::serde::{Deserialize, json::Json};
 use rocket::Rocket;
 use rocket::Build;
@@ -21,9 +19,9 @@ struct PhrasePair {
     phrase2: String,
 }
 
-#[post("/shane", data = "<task>")]
-fn edit_dist_from_json(task: Json<PhrasePair>) -> String {
-    format!("{}", edit_dist(&task.phrase1, &task.phrase2))
+#[post("/shane", data = "<phrases>")]
+fn edit_dist_from_json(phrases: Json<PhrasePair>) -> String {
+    format!("{}", edit_dist(&phrases.phrase1, &phrases.phrase2))
 }
 
 #[get("/<file..>")]
@@ -31,32 +29,10 @@ async fn files(file: PathBuf) -> Option<NamedFile> {
     NamedFile::open(Path::new("static/").join(file)).await.ok()
 }
 
-#[get("/world")]
-fn world() -> &'static str {
-    "Hello, World!"
-}
-
 #[get("/wait/<seconds>")]
 async fn wait(seconds: u64) -> String {
     sleep(Duration::from_secs(seconds)).await;
     format!("Waited {} seconds", seconds)
-}
-
-#[get("/blocking_task")]
-async fn blocking_task() -> io::Result::<Vec<u8>> {
-    let vec = spawn_blocking(|| std::fs::read("static/csgo_round_snapshots.csv")).await
-        .map_err(|e| io::Error::new(io::ErrorKind::Interrupted, e))??;
-
-    Ok(vec)
-}
-
-#[get("/<name>/<age>/<cool>")]
-fn hello(name: &str, age: u8, cool: bool) -> String {
-    if cool {
-        format!("{} is so cool and is {} years old", name, age)
-    } else {
-        format!("aw don't say that... just lie")
-    }
 }
 
 #[get("/shane/<p1>/<p2>")]
@@ -74,8 +50,7 @@ fn root() -> &'static str {
 #[launch]
 fn rocket() -> Rocket<Build> {
     rocket::build()
-        .mount("/hello", routes![world, wait, hello])
         .mount("/files", routes![files])
-        .mount("/", routes![root, get_edit_dist, edit_dist_from_json, blocking_task])
+        .mount("/", routes![wait, root, get_edit_dist, edit_dist_from_json])
 }
 
