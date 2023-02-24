b12d8c28b415f081d5d9aec4659a362d87ea9c91
reshane <shanekenny023@gmail.com>
2023-02-23 20:12:42 -0500

    Add poorly written & useless tests

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ use rocket::tokio::time::{sleep, Duration};
 use std::io;
 use rocket::tokio::task::spawn_blocking;
 use rocket::serde::{Deserialize, json::Json};
+use rocket::Rocket;
+use rocket::Build;
 
 use std::path::{Path, PathBuf};
 use rocket::fs::NamedFile;
@@ -22,11 +24,6 @@ struct PhrasePair {
 #[post("/shane", data = "<task>")]
 fn edit_dist_from_json(task: Json<PhrasePair>) -> String {
     format!("{}", edit_dist(&task.phrase1, &task.phrase2))
-}
-
-#[get("/")]
-fn root() -> &'static str {
-    "Shane was here"
 }
 
 #[get("/<file..>")]
@@ -67,14 +64,44 @@ fn get_edit_dist(p1: String, p2: String) -> String {
     format!("{}", edit_dist(&p1, &p2))
 }
 
-#[rocket::main]
-async fn main() -> Result<(), rocket::Error> {
-    let _rocket = rocket::build()
+#[get("/")]
+fn root() -> &'static str {
+    "Shane was here"
+}
+
+#[launch]
+fn rocket() -> Rocket<Build> {
+    rocket::build()
         .mount("/hello", routes![world, wait, hello])
         .mount("/files", routes![files])
         .mount("/", routes![root, get_edit_dist, edit_dist_from_json, blocking_task])
-        .launch()
-        .await?;
+}
 
-    Ok(())
+#[cfg(test)]
+mod test {
+    use super::rocket;
+    use rocket::local::blocking::Client;
+    use rocket::http::Status;
+
+    use crate::edit_dist::edit_dist;
+
+    #[test]
+    fn root() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let response = client.get(uri!(super::root)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), "Shane was here");
+    }
+
+    #[test]
+    fn get_edit_dist() {
+        let client = Client::tracked(rocket()).expect("valid rocket instance");
+        let phrase1: String = String::from("shane");
+        let phrase2: String = String::from("shane");
+        let diff: usize = edit_dist(&phrase1.clone(), &phrase2.clone());
+        let response = client
+            .get(format!("/shane/{}/{}", &phrase1, &phrase2)).dispatch();
+        assert_eq!(response.status(), Status::Ok);
+        assert_eq!(response.into_string().unwrap(), format!("{}", diff)); 
+    }
 }
